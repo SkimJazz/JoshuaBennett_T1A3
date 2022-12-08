e9a0e9e9d8de75054a61a073db61ab3a8838d5df
SkimJazz <jb.skimjazz@outlook.com>
2022-12-08 15:11:32 +1000

    initial commit

---
+++ hangman-main.py
@@ -0,0 +1,10 @@
+# 
+
+import sys
+# VS Code will ask which Python you would like to use when 'import sys'
+# is added to the python file.
+
+print(sys.path)
+# This will print out the import paths or where Python will look when
+# we try to import things. i.g. If we wanted to import the Request Libray
+# then Python will look for the Request Libray inside 'sys.path'
